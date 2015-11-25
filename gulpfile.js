5c65d6efaa5efa7b07c85e54c9087b0f9d976bdb
Maja Wichrowska <maja.wichrowska@airbnb.com>
2015-11-25 18:03:42 -0500

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+var htmlreplace = require('gulp-html-replace');
+var source = require('vinyl-source-stream');
+//var babelify = require('babelify')
+var browserify = require('browserify');
+var reactify = require('reactify');
+var watchify = require('watchify');
+var streamify = require('gulp-streamify');
+
+
+var path = {
+  HTML: 'src/index.html',
+  MINIFIED_OUT: 'build.min.js',
+  OUT: 'build.js',
+  DEST: 'dist',
+  DEST_BUILD: 'dist/build',
+  DEST_SRC: 'dist/src',
+  ENTRY_POINT: './src/js/main.js'
+};
+
+gulp.task('copy', function(){
+  gulp.src(path.HTML)
+    .pipe(gulp.dest(path.DEST));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(path.HTML, ['copy']);
+
+  var watcher  = watchify(browserify({
+    entries: [path.ENTRY_POINT],
+    transform: [reactify],
+    debug: true,
+    cache: {}, packageCache: {}, fullPaths: true
+  }));
+
+  return watcher.on('update', function () {
+    watcher.bundle()
+      .pipe(source(path.OUT))
+      .pipe(gulp.dest(path.DEST_SRC))
+      console.log('Updated');
+  })
+    .bundle()
+    .pipe(source(path.OUT))
+    .pipe(gulp.dest(path.DEST_SRC));
+});
+gulp.task('default', ['watch']);
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+console.log("Hello world")
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('wrapper')
+);
